f2508d08079a38b2a50259c9de283d7f93fa4b1f
Eddie Knight <knight@linux.com>
2025-08-31 10:44:08 -0500

    debugging?
    Signed-off-by: Eddie Knight <knight@linux.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,12 @@ if [ -z "$ACTIONS_RUNTIME_TOKEN" ]; then
     missing_vars=true
 fi
 
-if [ -z "$GITHUB_REPOSITORY_OWNER" ]; then
+if [ -z "$INPUT_REPO_OWNER" ]; then
     echo "Error: REPO_OWNER environment variable is required but not set"
     missing_vars=true
 fi
 
-if [ -z "$GITHUB_PATH" ]; then
+if [ -z "$INPUT_REPO_NAME" ]; then
     echo "Error: REPO_NAME environment variable is required but not set"
     missing_vars=true
 fi
@@ -25,12 +25,12 @@ if [ "$missing_vars" = true ]; then
 fi
 
 echo "INPUT_GH_TOKEN: $ACTIONS_RUNTIME_TOKEN"
-echo "GITHUB_REPOSITORY_OWNER: $GITHUB_REPOSITORY_OWNER"
-echo "GITHUB_PATH: $GITHUB_REPOSITORY"
+echo "GITHUB_REPOSITORY_OWNER: $INPUT_REPO_OWNER"
+echo "GITHUB_PATH: $INPUT_REPO_NAME"
 
 sed "s/{{ INPUT_GH_TOKEN }}/$ACTIONS_RUNTIME_TOKEN/g" /pvtr-config.yml
-sed "s/{{ GITHUB_REPOSITORY_OWNER }}/$GITHUB_REPOSITORY_OWNER/g" /pvtr-config.yml
-sed "s/{{ GITHUB_REPOSITORY }}/$GITHUB_REPOSITORY/g" /pvtr-config.yml
+sed "s/{{ GITHUB_REPOSITORY_OWNER }}/$INPUT_REPO_OWNER/g" /pvtr-config.yml
+sed "s/{{ GITHUB_REPOSITORY }}/$INPUT_REPO_NAME/g" /pvtr-config.yml
 
 # Execute the main privateer command with all provided arguments
 exec /bin/privateer run -b /bin/pvtr-plugins -c /pvtr-config.yml
